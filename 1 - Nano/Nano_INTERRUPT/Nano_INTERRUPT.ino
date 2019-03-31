4f049216d7fdd6d7cc5a1972f5ea4b4171f0e6b7
Roger-Takeshita <roger.takeshita@gmail.com>
2019-03-31 18:13:37 -0500

    Arduino Nano Interrupt Button

---
+++ 1 - Nano/Nano_INTERRUPT/Nano_INTERRUPT.ino
@@ -0,0 +1,47 @@
+//======================================================================VARS
+  int button1 = 32;
+  int button2 = 33;
+  int delayTime = 1000;
+  int updateButton = 150;
+  volatile int btn1, btn2;
+  volatile bool buttonPressedFlag = false;
+  unsigned long lastT_Button = millis();
+
+//======================================================================INTERRUPT
+  void interruptButtonPressed() {
+    btn1 = btn2 = 0;
+    btn1 = digitalRead(button1);
+    btn2 = digitalRead(button2);
+    if (btn1 > 0 || btn2 > 0) {
+      buttonPressedFlag = true;
+    }
+  }
+//======================================================================SETUP
+  void setup() {
+    //Serial.begin(9600);
+    Serial.begin(115200);
+    pinMode(button1, INPUT);
+    pinMode(button2, INPUT);
+    attachInterrupt (button1, interruptButtonPressed, RISING);
+    attachInterrupt (button2, interruptButtonPressed, RISING);
+  }
+
+//======================================================================LOOP
+  void loop() {
+    //-------------------------------------------------------LOOP Interrupt Buttons
+      if (buttonPressedFlag) {
+        if (millis() - lastT_Button > updateButton){
+          if (btn1 > 0) {
+            Serial.println("Button 1 was pressed");
+          }
+          if (btn2 > 0) {
+            Serial.println("Button 2 was pressed");
+          }
+          lastT_Button = millis();
+        }
+        buttonPressedFlag = false;
+      }
+      
+    //---------------------------------------------------------OTHER
+  }
+  
